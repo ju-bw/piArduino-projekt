51604d3ab25bdfd050e57e79b0e2f221b296c7dc
Jan Unger <info@bw1.eu>
2018-10-27 02:14:43 +0200

    led pi python bcm

--- pi/piKofler/python/rpigpio-version.py
+++ pi/piKofler/python/rpigpio-version.py
@@ -3,8 +3,5 @@ import RPi.GPIO as GPIO
 print(GPIO.VERSION)
 print(GPIO.RPI_INFO)
 
-##!/usr/bin/python
-#import RPi.GPIO as gpio, sys, time
-#print 'Pi-Info', gpio.RPI_INFO['P1_REVISION']
 
 
--- pi/piKofler/python/tkinter-led-ein-aus.py
+++ pi/piKofler/python/tkinter-led-ein-aus.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 from tkinter import *
 import RPi.GPIO as gpio
-gpio.setmode(gpio.BOARD)
-gpio.setwarnings(False)  
-gpio.setup(35, gpio.OUT)   # Pin 35 
-gpio.output(35, gpio.LOW)
+#gpio.setmode(gpio.BOARD)
+gpio.setmode(gpio.BCM)
+
+ledPin = 19
+gpio.setup(ledPin, gpio.OUT)  
+gpio.output(ledPin, gpio.LOW)
 
 # Reaktion auf Mausklick im Fenster
 def led_change():
   if ledstatus.get():
-    gpio.output(35, gpio.HIGH)  
+    gpio.output(ledPin, gpio.HIGH)  
     lbl.configure(text='Die LED leuchtet.')
   else:
-    gpio.output(35, gpio.LOW)  
+    gpio.output(ledPin, gpio.LOW)  
     lbl.configure(text='Die LED ausgeschalten.')
 
 # Benutzeroberfläche mit Ereignisverwaltung
@@ -25,4 +27,13 @@ ledbtn = Checkbutton(mywin, text='LED ein-/ausschalten',
                      command=led_change, padx=10, pady=10)
 lbl.grid(column=0, row=0, padx=5, pady=5)
 ledbtn.grid(column=0, row=1, padx=5, pady=5)
-mywin.mainloop()
+
+try:         		  
+  while True:       
+    mywin.mainloop()
+
+finally:
+  print("Cleaning up")
+  GPIO.cleanup()
+
+
